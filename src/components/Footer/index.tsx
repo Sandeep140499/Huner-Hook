bd42ef35b0b5ae51188cc443122e7c60c1761ee7
Sandeep994014 <sandeep.kr.chaurasiya@kibalabs.in>
2024-12-19 13:58:48 +0530

    footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -19,6 +19,7 @@ import {
   Label,
   // LanguageSwitch,
   LanguageSwitchContainer,
+  FooterContainer,
 } from "./styles";
 
 interface SocialLinkProps {
@@ -132,36 +133,31 @@ const Footer = ({ t }: { t: TFunction }) => {
 </LogoContainer>
 
             </NavLink>
-            {/* <FooterContainer>
-              <SocialLink
-                href="https://github.com/Adrinlol/create-react-app-adrinlol"
-                src="./"
-              />
-              <SocialLink
-                href="https://twitter.com/Adrinlolx"
-                src="twitter.svg"
-              />
-              <SocialLink
-                href="https://www.linkedin.com/in/lasha-kakabadze/"
-                src="linkedin.svg"
-              />
-              <SocialLink
-                href="https://medium.com/@lashakakabadze/"
-                src="medium.svg"
-              />
-              <a
-                href="https://ko-fi.com/Y8Y7H8BNJ"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  height="36"
-                  style={{ border: 0, height: 36 }}
-                  src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
-                  alt="Buy Me a Coffee at ko-fi.com"
-                />
-              </a>
-            </FooterContainer> */}
+            <FooterContainer>
+  <SocialLink
+    href="https://github.com/Adrinlol/create-react-app-adrinlol"
+    src="./"
+  />
+  <SocialLink href="https://twitter.com/Adrinlolx" src="twitter.svg" />
+  <SocialLink
+    href="https://www.linkedin.com/in/lasha-kakabadze/"
+    src="linkedin.svg"
+  />
+  <SocialLink href="https://medium.com/@lashakakabadze/" src="medium.svg" />
+  <a
+    href="https://ko-fi.com/Y8Y7H8BNJ"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img
+      height="36"
+      style={{ border: 0, height: 36 }}
+      src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
+      alt="Buy Me a Coffee at ko-fi.com"
+    />
+  </a>
+</FooterContainer>
+
 
             <p>
             <footer>
